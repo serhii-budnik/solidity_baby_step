55b6fae047292c3796493f5e5b252821ee029e46
Serhii Budnik <serhiibudnik@gmail.com>
2018-06-03 14:46:12 +0300

    split spec files

--- first_project/test/spec/VotingSpec.js
+++ first_project/test/spec/VotingSpec.js
@@ -2,43 +2,6 @@ import testHelper from '../testHelper.js';
 
 var Voting = artifacts.require("Voting");
 
-contract('Voting', function(accounts) {
-  it('should set maxVotes value of 2', function() {
-    return Voting.deployed().then(function(instance){
-      return instance.maxVotes(); //.call(accounts[0]);
-    }).then(function(maxVotes){
-      assert.equal(maxVotes, 2, "2 wasn't in the contract");
-    });
-  });
-
-  it('should set id value of 1', function(){
-    return Voting.deployed().then(function(instance){
-      console.log("instance.id", instance.id());
-      return instance.id();
-    }).then(function(id) {
-      assert.equal(id, 1, "ID wasn't 1 in the contract")
-    });
-  });
-
-  it('should add proposal to array proposals', function() {
-    return Voting.deployed().then(function(instance) {
-      var votingInst = instance;
-      instance.addProposal("Serhii");//.call(accounts[0]);
-      return votingInst;
-    }).then(function(instance) {
-      return instance.proposals(0);
-    }).then(function(proposal) {
-      // Here get first element from array Struct(name, voteCount);
-      assert.equal(proposal[0], 'Serhii', 'Name is not a Serhii');
-      assert.equal(proposal[1], 0, 'voteCount is not a zero');
-    });
-  });
-});
-
-/********************
- * Using ASYNC/AWAIT *
-********************/
-
 contract('2nd Voting Test', async (accounts) => {
   it('should set for maxVotes value 2', async() => {
     let instance = await Voting.deployed();
